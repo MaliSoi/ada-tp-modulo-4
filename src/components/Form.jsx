1438dffdd60ea4f43126117b0cc8fa3d8e3a1df5
MaliSoi <malalasoifer@gmail.com>
2025-12-03 11:00:49 -0300

    feat:Crear From.jsx

---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+import {useState} from "react";
+import { Button, HStack, Input } from "@chakra-ui/react";
+
+const Form = ({addTodo}) => {
+    const [input, setInput] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!input.trim()) return;
+        addTodo(input);
+        setInput("");
+    };
+
+    return (
+        <HStack as="form" onSubmit={handleSubmit} spacing={3}>
+            <Input
+            placeholder="Nueva Tarea"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            />
+            <Button type="submit" colorScheme="blue">
+                Agregar Tarea
+            </Button>
+        </HStack>
+
+    );
+};
+
+export default Form;
