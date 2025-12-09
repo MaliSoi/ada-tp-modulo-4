9079506cd4a3adb46cef69cf43d428a7a75e4a54
MaliSoi <malalasoifer@gmail.com>
2025-12-09 14:40:50 -0300

    Agregar validación al formulario de nuevas tareas y mostrar mensaje de error

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,50 @@
 import {useState} from "react";
-import { Button, HStack, Input } from "@chakra-ui/react";
+import { Button, VStack, Input, FormControl, FormErrorMessage, Text } from "@chakra-ui/react";
 
 
 
 const Form = ({addTodo}) => {
     const [input, setInput] = useState("");
+    const [error, setError] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!input.trim()) return;
-        addTodo(input);
-        setInput("");
+
+        if (!input.trim()) {
+      setError("La tarea no puede estar vacía");
+      return;
+    }
+
+    setError("");
+    addTodo(input);
+    setInput("");
     };
 
     return (
-        <HStack as="form" onSubmit={handleSubmit} spacing={3}>
+        <VStack as="form" onSubmit={handleSubmit} spacing={3} w="100%">
+            
+            <FormControl isInvalid={!!error} W="100%">
+            <VStack spacing={1} align="start" w="100%">
             <Input
             placeholder="Nueva Tarea"
             value={input}
-            onChange={(e) => setInput(e.target.value)}
-            />
+            onChange={(e) => {
+                setInput(e.target.value);
+                if (error) setError(""); // limpiar error al escribir
+          }}
+          />
+            
+            <FormErrorMessage minH="20px">
+            {error}
+            </FormErrorMessage>
+            </VStack>
+            </FormControl>
+
             <Button type="submit" colorScheme="blue" size="md" px={8} py={3} >
-                Agregar Tarea
+            Agregar Tarea
             </Button>
-        </HStack>
+             
+            </VStack>
 
     );
 };
